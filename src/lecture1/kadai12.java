dbcd0170f5e48f796257911e417e7eff32047853
kawai <b2210720@photon.chitose.ac.jp>
2022-11-02 12:30:55 +0900

    kadai12

---
+++ src/lecture1/kadai12.java
@@ -0,0 +1,13 @@
+package lecture1;
+import java.util.Scanner;
+public class kadai12 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int studentNumber;
+
+        System.out.print("数値を入力:");
+        studentNumber = scanner.nextInt();
+
+        System.out.println("B" + studentNumber + " Kawai Shimon");
+    }
+}
