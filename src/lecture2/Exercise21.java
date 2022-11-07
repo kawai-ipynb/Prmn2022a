97b8159ea615eb8d02db7aad5c74e8d337819c33
kawai <b2210720@photon.chitose.ac.jp>
2022-11-07 22:21:41 +0900

    kadai1

---
+++ src/lecture2/Exercise21.java
@@ -0,0 +1,10 @@
+package lecture2;
+
+public class Exercise21 {
+    public static void main(String[] args) {
+        Human h1 = new Human("たけし",12);
+        h1.print();
+        Human h2 = new Human("さとし",22);
+        h2.print();
+    }
+}
