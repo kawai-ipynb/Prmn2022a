effb53f6be42a4efa43fdc0ee123f64a86834598
kawai <b2210720@photon.chitose.ac.jp>
2022-11-07 23:43:59 +0900

    exercise22 が邪魔だったのでいったん削除してから追加した

--- src/lecture2/Exercise22.java
+++ src/lecture2/Exercise22.java
@@ -2,7 +2,7 @@ package lecture2;
 
 public class Exercise22 {
     public static void main(String[] args) {
-        Car c = new Car(car,engine);
+        Car c = new Car();
         GasStation g = new GasStation();
         c.run();
         g.refuel(c);
