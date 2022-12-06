6a9b6fab05b182a6b266f2103ab008f0d74babac
kawai <b2210720@photon.chitose.ac.jp>
2022-12-07 07:44:20 +0900

    62

---
+++ src/lecture6/Exercise62.java
@@ -0,0 +1,29 @@
+package lecture6;
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Exercise62 {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        ArrayList<Integer> list = new ArrayList<>();
+        Scanner inp = new Scanner(System.in);
+
+        for (int i = 0; i < 5; i++) {
+            list.add(rand.nextInt(6) + 1);
+        }
+
+        System.out.println("さいころを5つふりました");
+
+        try {
+            System.out.print("何番目のさいころの値を確認しますか？:");
+            int index = inp.nextInt();
+            System.out.println(index + "番目のさいころの値:" + list.get(index));
+        }
+        catch (IndexOutOfBoundsException e) {
+            System.out.println("list範囲外のアクセスを確認しました");
+            System.out.println("終了します");
+        }
+
+
+    }
+}
