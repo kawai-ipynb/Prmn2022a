86c72090731c26bf026774dd3d7675917b946f4c
kawai <b2210720@photon.chitose.ac.jp>
2022-12-07 06:57:07 +0900

    56

---
+++ src/lecture5/Exercise56.java
@@ -0,0 +1,8 @@
+package lecture5;
+
+public class Exercise56 {
+    public static void main(String[] args) {
+        Insect insect = new SwallowtailButterfly();
+        insect.move();
+    }
+}
---
+++ src/lecture5/SwallowtailButterfly.java
@@ -0,0 +1,8 @@
+package lecture5;
+
+public class SwallowtailButterfly extends Butterfly{
+    @Override
+    public void move() {
+        System.out.println("綺麗に飛んだよ");
+    }
+}
