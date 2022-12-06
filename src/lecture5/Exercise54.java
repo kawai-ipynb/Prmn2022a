d0346342ecc5872bf17c401bbfca1b7c2f556170
kawai <b2210720@photon.chitose.ac.jp>
2022-12-07 06:52:49 +0900

    54

---
+++ src/lecture5/Exercise54.java
@@ -0,0 +1,8 @@
+package lecture5;
+
+public class Exercise54 {
+    public static void main(String[] args) {
+        Locust locust = new Locust();
+        locust.move();
+    }
+}
---
+++ src/lecture5/Locust.java
@@ -0,0 +1,8 @@
+package lecture5;
+
+public class Locust extends Insect{
+    @Override
+    public void move() {
+        System.out.println("跳んだよ");
+    }
+}
